e75591eba55ea91360e29964a16723e8fd8d7ce3
Jaco Marais <jaco@remotech.co.za>
2025-03-18 16:47:05 +0200

    added backend proxy /backend

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://api.clarifai.com',
+      '/backend': {
+        target: 'https://facedetectionappbackend.onrender.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/backend/, ''),
         secure: false,
       },
     },
